0f0c3ea54faebc5c50be77b9bfc902dd3b16af42
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-08 14:13:44 +0530

    assignment 1

--- Assignment1.py
+++ Assignment1.py
@@ -1,14 +1,21 @@
 import cv2
 
-image = cv2.imread("image_2.jpg")
+path = input("Enter the path of image: ")
+image = cv2.imread(path)
 
 if image is not None:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Gray Image", gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    success = cv2.imwrite("gray_image.jpg", gray)
-    if success:
-        print("Gray image saved successfully")
-    else : print("Failed to save gray image")
-else : print("Image not found")
+    choice = input("Type 'save' to save image or 'show' to show the image: ")
+    
+    if choice == "show":
+        cv2.imshow("Gray Image", gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    elif choice == "save":
+        save = input("Enter the name to save the image (include .png or .jpg): ")
+        cv2.imwrite(save, gray)
+        print(f"Image saved as {save}")
+    else:
+        print("Invalid choice")
+else:
+    print("Image not found. Check the path and try again.")
